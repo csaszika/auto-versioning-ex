f6c43e3455c6992d02d9fadab19ef100dc4bd3fb
csaszika <csaszika4@gmail.com>
2022-12-28 19:37:39 +0100

    Initial commit

---
+++ apps/example-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auto-versioning-ex-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'example-app';
+}
